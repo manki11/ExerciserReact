31e7b520ff0475bf9a368a80879162c6ef763e2f
Mankirat Singh <mankirat1998@gmail.com>
2018-06-17 17:50:04 +0530

    feat: answers in mcq form made dynamic

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -13,7 +13,7 @@ class Exercise extends Component {
         }
 
         return (
-            <div className="col-md-6">
+            <div className="col-md-5">
                 <div className="card">
                     <div className="exercise-card-content">
                         <h3 className="card-title">{title}</h3>
--- src/containers/MCQForm.js
+++ src/containers/MCQForm.js
@@ -9,72 +9,80 @@ class MCQForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            title:'',
+            title: '',
             noOfQuestions: 0,
-            currentQuestionNo:-1,
+            currentQuestionNo: -1,
             questions: [],
             currentQuestion: {
-                id: -1,
+                id: 1,
                 question: "",
-                a: "",
-                b: "",
-                c: "",
-                d: "",
-                correctAns: ""
+                answers: ['', ''],
             }
         }
     }
+
+    handleChangeAns = e => {
+        const index = Number(e.target.name.split('-')[1]);
+        const ans = this.state.currentQuestion.answers.map((ans, i) => (
+            i === index ? e.target.value : ans
+        ));
+        this.setState({...this.state, currentQuestion: {...this.state.currentQuestion, answers: ans}})
+    };
+
+    handleRemoveAns = e => {
+        const {currentQuestion} = this.state;
+        const {answers} = currentQuestion;
+        if (answers.length > 2) {
+            answers.pop();
+            this.setState({currentQuestion: {...currentQuestion, answers: answers}})
+        }
+    };
+
+    handleNewAns = e => {
+        const {currentQuestion} = this.state;
+        this.setState({currentQuestion: {...currentQuestion, answers: [...this.state.currentQuestion.answers, '']}})
+    };
 
     handleNewEvent = event => {
         event.preventDefault();
         this.saveCurrentForm();
     };
 
-    saveCurrentForm= ()=> {
-        let a= this.state.currentQuestion.a;
-        let b= this.state.currentQuestion.b;
-        let c= this.state.currentQuestion.c;
-        let d= this.state.currentQuestion.d;
+    saveCurrentForm = () => {
+        const {question, answers} = this.state.currentQuestion;
+        let correctAns = answers[0];
+        let id = this.state.noOfQuestions + 1;
 
-        let question= this.state.currentQuestion.question;
-        let answers=[a,b,c,d];
-        let id= this.state.noOfQuestions+1;
-        let correctAns= a;
-
-        let newQues={
-            id:id,
-            answers:answers,
-            question:question,
-            correctAns:correctAns
+        let newQues = {
+            id: id,
+            answers: answers,
+            question: question,
+            correctAns: correctAns
         };
 
         this.setState({
             ...this.state,
-            questions:[
+            questions: [
                 ...this.state.questions,
                 newQues
             ],
             noOfQuestions: id,
-            currentQuestionNo: id+1,
+            currentQuestionNo: id + 1,
             currentQuestion: {
                 id: -1,
                 question: "",
-                a: "",
-                b: "",
-                c: "",
-                d: "",
-                correctAns: ""
+                answers: ['', ''],
             }
         });
     };
 
-    submitExercise= ()=> {
-        let exercise={
-            title: this.state.title ,
-            id: this.props.counter+1,
+    submitExercise = () => {
+        let exercise = {
+            title: this.state.title,
+            id: this.props.counter + 1,
             type: "mcq",
             questions: this.state.questions,
-            scores:[]
+            scores: []
         };
 
         this.props.addNewExercise(exercise);
@@ -82,7 +90,32 @@ class MCQForm extends Component {
 
     };
 
+    previousQues = () => {
+
+    };
+
     render() {
+        const {currentQuestion} = this.state;
+        let inputs = currentQuestion.answers.map((ans, i) => {
+            let placeholder = 'Wrong Option';
+            if (i === 0) placeholder = 'Correct Option';
+            return (
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="form-group" key={`answers-${i}`}>
+                            <label htmlFor={`answer-${i}`}>{i + 1}</label>
+                            <input
+                                className="form-control"
+                                name={`answer-${i}`}
+                                type="text"
+                                value={ans}
+                                placeholder={placeholder}
+                                onChange={this.handleChangeAns}/>
+                        </div>
+                    </div>
+                </div>
+            )
+        });
         return (
             <div className="container-fluid">
                 <div className="row align-items-center justify-content-center">
@@ -99,7 +132,7 @@ class MCQForm extends Component {
                                             value={this.state.title}
                                             onChange={e => this.setState({
                                                 ...this.state,
-                                                title:e.target.value
+                                                title: e.target.value
                                             })}
                                         />
                                     </div>
@@ -122,92 +155,40 @@ class MCQForm extends Component {
                                         />
                                     </div>
                                 </div>
+                                {inputs}
                                 <div className="row">
-                                    <div className="col-md-6">
-                                        <div className="form-group">
-                                            <label htmlFor="correctanswer">Correct Answer:</label>
-                                            <input
-                                                className="form-control"
-                                                type="text"
-                                                id="correctanswer"
-                                                value={this.state.currentQuestion.a}
-                                                onChange={e => this.setState({
-                                                    currentQuestion: {
-                                                        ...this.state.currentQuestion,
-                                                        a: e.target.value
-                                                    }
-                                                })}
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-md-6">
-                                        <div className="form-group">
-                                            <label htmlFor="wronganswer">Other Options:</label>
-                                            <input
-                                                className="form-control"
-                                                type="text"
-                                                id="wronganswer"
-                                                value={this.state.currentQuestion.b}
-                                                onChange={e => this.setState({
-                                                    currentQuestion: {
-                                                        ...this.state.currentQuestion,
-                                                        b: e.target.value
-                                                    }
-                                                })}
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-md-6">
-                                        <div className="form-group">
-                                            <input
-                                                className="form-control"
-                                                type="text"
-                                                value={this.state.currentQuestion.c}
-                                                onChange={e => this.setState({
-                                                    currentQuestion: {
-                                                        ...this.state.currentQuestion,
-                                                        c: e.target.value
-                                                    }
-                                                })}
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-md-6">
-                                        <div className="form-group">
-                                            <input
-                                                className="form-control"
-                                                type="text"
-                                                value={this.state.currentQuestion.d}
-                                                onChange={e => this.setState({
-                                                    currentQuestion: {
-                                                        ...this.state.currentQuestion,
-                                                        d: e.target.value
-                                                    }
-                                                })}
-                                            />
-                                        </div>
+                                    <div className="form-group">
+                                        <button
+                                            type="button"
+                                            onClick={this.handleNewAns}
+                                            className="btn btn-dark">
+                                            +
+                                        </button>
+                                        <button
+                                            type="button"
+                                            onClick={this.handleRemoveAns}
+                                            className="btn btn-dark">
+                                            -
+                                        </button>
                                     </div>
                                 </div>
                                 <div className="row justify-content-between">
-                                    <button className={"btn btn-info"+(this.state.noOfQuestions>=1 ? '' : 'disabled')}>
+                                    <button
+                                        className={"btn btn-info" + (this.state.noOfQuestions >= 1 ? '' : 'disabled')}
+                                        onClick={this.previousQues}
+                                    >
                                         Previous Question
                                     </button>
                                     <div className="justify-content-end">
-                                    <button type="submit" className="btn btn-info submit-button">
-                                        Next Question
-                                    </button>
-                                    <button
-                                        className={"btn btn-success"+(this.state.noOfQuestions>=2 ? '' : 'disabled')}
-                                        onClick={this.submitExercise}
-                                    >
-                                        Finish Exercise
-                                    </button>
+                                        <button type="submit" className="btn btn-info submit-button">
+                                            Next Question
+                                        </button>
+                                        <button
+                                            className={"btn btn-success" + (this.state.noOfQuestions >= 2 ? '' : 'disabled')}
+                                            onClick={this.submitExercise}
+                                        >
+                                            Finish Exercise
+                                        </button>
                                     </div>
                                 </div>
                             </form>
