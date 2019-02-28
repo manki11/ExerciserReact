9ff2f5c2adc21b5442dcdb168c1f5a2e885f6bd5
NikhilM98 <nikhilmehra998@gmail.com>
2019-02-28 20:29:17 +0530

    Unused parameters removed from routes

--- src/containers/Router.js
+++ src/containers/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Switch, Route, withRouter, Redirect} from "react-router-dom";
+import {Switch, Route, withRouter} from "react-router-dom";
 import {connect} from "react-redux";
 import ExerciseList from "./ExerciseList";
 import Scores from "./Scores/Scores"
@@ -15,36 +15,34 @@ import REORDERPlay from "./Players/REORDERPlayer"
 import {injectIntl} from 'react-intl';
 import '../css/index.css';
 
-
 import NewExerciseTemplate from "./Builders/Template";
 import PresenceScores from "./Scores/PresenceScores";
-const Main= props=> {
+const Main = props => {
     const {onUpdate, onSharedResult}= props;
 
     return(
         <div className="main-container">
             <Switch>
                 <Route exact path="/" render={props=> <ExerciseList onUpdate={onUpdate} {...props}/>}/>
-                <Route exact path="/new" render={props=> <NewExerciseTemplate/>} {...props}/>
-                <Route exact path="/scores" render={props=> <Scores onSharedResult={onSharedResult}/>} {...props}/>
+                <Route exact path="/new" component={NewExerciseTemplate} {...props}/>
+                <Route exact path="/scores" render={()=> <Scores onSharedResult={onSharedResult}/>} {...props}/>
 
                 // MCQ
-                <Route exact path="/new/mcq" render={props=> <MCQForm/>} {...props}/>
-                <Route exact path="/edit/mcq" render={props=> <MCQForm/>} {...props}/>
-                <Route exact path="/play/mcq" render={props=> <MCQPlay/>} {...props}/>
+                <Route exact path="/new/mcq" component={MCQForm} {...props}/>
+                <Route exact path="/edit/mcq" component={MCQForm} {...props}/>
+                <Route exact path="/play/mcq" component={MCQPlay} {...props}/>
 
                 // CLOZE
-                <Route exact path="/new/cloze" render={props=> <CLOZEForm/>} {...props}/>
-                <Route exact path="/edit/cloze" render={props=> <CLOZEForm/>} {...props}/>
-                <Route exact path="/play/cloze" render={props=> <CLOZEPlay/>} {...props}/>
+                <Route exact path="/new/cloze" component={CLOZEForm} {...props}/>
+                <Route exact path="/edit/cloze" component={CLOZEForm} {...props}/>
+                <Route exact path="/play/cloze" component={CLOZEPlay} {...props}/>
 
                 // REORDER
-                <Route exact path="/new/reorder" render={props=> <REORDERForm/>} {...props}/>
-                <Route exact path="/edit/reorder" render={props=> <REORDERForm/>} {...props}/>
-                <Route exact path="/play/reorder" render={props=> <REORDERPlay/>} {...props}/>
+                <Route exact path="/new/reorder" component={REORDERForm} {...props}/>
+                <Route exact path="/edit/reorder" component={REORDERForm} {...props}/>
+                <Route exact path="/play/reorder" component={REORDERPlay} {...props}/>
 
-                <Route exact path="/presence/scores" render={props=> <PresenceScores/>} {...props}/>
-
+                <Route exact path="/presence/scores" component={PresenceScores} {...props}/>
             </Switch>
         </div>
     )
