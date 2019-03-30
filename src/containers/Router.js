396d2d79b59092b704d62b94ff9df53e11540f04
Anshuman Bhardwaj <anshu5074@gmail.com>
2019-03-30 14:59:09 +0530

    Remove conflicts with remote branch
    Remove redundant mapStatetoProps and connect

--- src/containers/Router.js
+++ src/containers/Router.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {Switch, Route, withRouter} from "react-router-dom";
-import {connect} from "react-redux";
 import ExerciseList from "./ExerciseList";
 import Scores from "./Scores/Scores"
 
@@ -19,41 +18,38 @@ import '../css/index.css';
 import NewExerciseTemplate from "./Builders/Template";
 import PresenceScores from "./Scores/PresenceScores";
 const Main= props=> {
+    console.log({props})
     const {onUpdate, onSharedResult}= props;
 
     return(
         <div className="main-container">
             <Switch>
                 <Route exact path="/" render={props=> <ExerciseList onUpdate={onUpdate} {...props}/>}/>
-                <Route exact path="/new" render={props=> <NewExerciseTemplate/>} {...props}/>
-                <Route exact path="/scores" render={props=> <Scores onSharedResult={onSharedResult}/>} {...props}/>
+                <Route exact path="/new" render={props=> <NewExerciseTemplate {...props}/>} />
+                <Route exact path="/scores" render={props=> <Scores onSharedResult={onSharedResult} {...props}/>} />
 
                 {/* // MCQ */}
-                <Route exact path="/new/mcq" render={props=> <MCQForm/>} {...props}/>
-                <Route exact path="/edit/mcq" render={props=> <MCQForm/>} {...props}/>
-                <Route exact path="/play/mcq" render={props=> <MCQPlay/>} {...props}/>
+                <Route exact path="/new/mcq" render={props=> <MCQForm {...props}/>} />
+                <Route exact path="/edit/mcq" render={props=> <MCQForm {...props}/>} />
+                <Route exact path="/play/mcq" render={props=> <MCQPlay {...props}/>} />
 
                 {/* // CLOZE */}
-                <Route exact path="/new/cloze" render={props=> <CLOZEForm/>} {...props}/>
-                <Route exact path="/edit/cloze" render={props=> <CLOZEForm/>} {...props}/>
-                <Route exact path="/play/cloze" render={props=> <CLOZEPlay/>} {...props}/>
+                <Route exact path="/new/cloze" render={props=> <CLOZEForm {...props}/>} />
+                <Route exact path="/edit/cloze" render={props=> <CLOZEForm {...props}/>} />
+                <Route exact path="/play/cloze" render={props=> <CLOZEPlay {...props}/>} />
 
                 {/* // REORDER */}
-                <Route exact path="/new/reorder" render={props=> <REORDERForm/>} {...props}/>
-                <Route exact path="/edit/reorder" render={props=> <REORDERForm/>} {...props}/>
-                <Route exact path="/play/reorder" render={props=> <REORDERPlay/>} {...props}/>
+                <Route exact path="/new/reorder" render={props=> <REORDERForm {...props}/>} />
+                <Route exact path="/edit/reorder" render={props=> <REORDERForm {...props}/>} />
+                <Route exact path="/play/reorder" render={props=> <REORDERPlay {...props}/>} />
 
-                <Route exact path="/presence/scores" render={props=> <PresenceScores/>} {...props}/>
+                <Route exact path="/presence/scores" render={props=> <PresenceScores {...props}/>} />
 
             </Switch>
         </div>
     )
 };
 
-function MapStateToProps(state) {
-    return {
 
-    }
-}
 
-export default injectIntl(withRouter(connect(MapStateToProps)(Main)));
+export default injectIntl(withRouter(Main));
