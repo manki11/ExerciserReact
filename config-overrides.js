d7a5326568fd4df2443ed6142f95f5e0a9069d23
AvinashAgarwal14 <agarwalavinash14@gmail.com>
2019-05-29 14:27:05 +0530

    dependency fix

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,18 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = function override(config, env) {
-    var aliasObj = config.resolve.alias;
-    aliasObj['webL10n'] = 'lib/webL10n';
-    aliasObj['sugar-web'] = 'lib/sugar-web';
-    aliasObj['mustache'] = 'lib/mustache';
-    aliasObj['picoModal'] = 'lib/picoModal';    
-    config.resolve.alias = aliasObj;
+    let resolvePlugin = (config.resolveLoader?config.resolveLoader: {});
+    resolvePlugin['alias']= {
+      'text': 'text-loader'
+    };
+    config['resolveLoader'] = resolvePlugin;
+
+    let resolvePath = config.resolve.alias;
+    resolvePath['webL10n'] = 'lib/webL10n';
+    resolvePath['sugar-web'] = 'lib/sugar-web';
+    resolvePath['mustache'] = 'lib/mustache';
+    resolvePath['picoModal'] = 'lib/picoModal';    
+    config.resolve.alias = resolvePath;
+
     return config;
 }
