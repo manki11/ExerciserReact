7d919331e3f436dc99a56f788a3dbcead4b579a5
AvinashAgarwal14 <agarwalavinash14@gmail.com>
2019-06-27 00:34:02 +0530

    detailed results for MCQ added

--- src/containers/Scores/PresenceScores.js
+++ src/containers/Scores/PresenceScores.js
@@ -5,7 +5,7 @@ import {connect} from "react-redux";
 import {injectIntl} from "react-intl";
 import {SCORES, TIME, YOUR_RESULTS} from "../translation";
 import "../../css/PresenceScores.css"
-
+import withScoreHOC from './ScoreHoc';
 
 class PresenceScores extends Component {
 
@@ -14,10 +14,15 @@ class PresenceScores extends Component {
 
         let {intl} = this.props;
         this.intl = intl;
+        this.modes = {
+            SCORE: 'score',
+            TIME: 'time',
+            DETAILS: 'details'
+        };
 
         this.state = {
-            score: true,
-            time: false,
+            mode: this.modes.SCORE,
+            userDetailsIndex: 0,
             chartScores: {
                 chartData: {},
                 options: {
@@ -145,7 +150,7 @@ class PresenceScores extends Component {
             times.push(result.time);
         });
 
-        if(score) {
+        if(this.state.mode === this.modes.SCORE) {
             this.setState({
                 ...this.state,
                 chartScores: {
@@ -164,7 +169,7 @@ class PresenceScores extends Component {
                     }
                 }
             })
-        }else{
+        }else if (this.state.mode === this.modes.TIME){
             this.setState({
                 ...this.state,
                 chartTimes: {
@@ -188,8 +193,7 @@ class PresenceScores extends Component {
 
     score = () => {
         this.setState({
-            score: true,
-            time: false
+            mode: this.modes.SCORE
         },()=>{
             this.setChart();
         })
@@ -197,32 +201,92 @@ class PresenceScores extends Component {
 
     time = () => {
         this.setState({
-            score: false,
-            time: true
+            mode: this.modes.TIME
         },()=>{
             this.setChart();
         })
     };
 
-    render() {
+    details = (event) => {
+        if(event.length!==0) {
+            if(this.props.location.state.exercise.type == 'MCQ'){
+                this.setState({
+                    userDetailsIndex: event[0]['_index'],
+                    mode: this.modes.DETAILS
+                })
+            }
+        }
+    };
 
+    render() {
+        const {getResultsTableElement, getWrongRightMarker} = this.props;
         let score_active = "";
         let time_active = "";
+        let chart="";
 
-        if (this.state.score)
+        if (this.state.mode === this.modes.SCORE){
             score_active = "active";
-        else
+            chart = (<Bar data={this.state.chartScores.chartData} getElementAtEvent={this.details} options={this.state.chartScores.options}/>);
+        }
+        else if (this.state.mode === this.modes.TIME) {
             time_active = "active";
+            chart = (<Bar data={this.state.chartTimes.chartData} options={this.state.chartTimes.options}/>);
+        }
+        else if (this.state.mode === this.modes.DETAILS) {
+
+            const {exercise} = this.props.location.state;
+            const {shared_results} = exercise;
+            let users = [];
+            let allUserAnswers = [];
+            shared_results.map((result) => {
+                users.push(result.user.name);
+                allUserAnswers.push(result.userAnswers);
+            });
+
+            let userAnswers = allUserAnswers[this.state.userDetailsIndex];
+            let resultDetails = userAnswers.map((answer, index) => {
+                return (
+                    <tr key={index}>
+                        <td>
+                            {getResultsTableElement(answer.question)}
+                        </td>
+                        <td>
+                            {getResultsTableElement(answer.correctAns)}
+                        </td> 
+                        <td>
+                            <div className="td-userans">
+                                {getResultsTableElement(answer.userAns)}
+                                <div style={{marginLeft: '5px'}}>
+                                    {getWrongRightMarker(answer)}
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                );
+            });
+
+            chart = (
+                <div>
+                    <br></br>
+                    <br></br>
+                    <table style={{width:'100%'}}>
+                        <thead>
+                            <tr>
+                                <th>Question</th>
+                                <th>Correct Answer</th> 
+                                <th>{users[this.state.userDetailsIndex]}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {resultDetails}
+                        </tbody> 
+                    </table>
+                </div>
+            );
+        }
 
         let score = (<button type="button" className={"score-button " + score_active} onClick={this.score}/>);
         let time = (<button type="button" className={"time-button " + time_active} onClick={this.time}/>);
-
-        let chart="";
-
-        if(this.state.score)
-            chart= (<Bar data={this.state.chartScores.chartData} options={this.state.chartScores.options}/>);
-        else
-            chart= (<Bar data={this.state.chartTimes.chartData} options={this.state.chartTimes.options}/>);
 
         return (
             <div className="container">
@@ -242,5 +306,5 @@ function MapStateToProps(state) {
     return {}
 }
 
-export default injectIntl(withRouter(
-    connect(MapStateToProps, {})(PresenceScores)));
+export default withScoreHOC()(injectIntl(withRouter(
+    connect(MapStateToProps, {})(PresenceScores))));
--- src/containers/Scores/Scores.js
+++ src/containers/Scores/Scores.js
@@ -6,7 +6,7 @@ import {injectIntl} from 'react-intl';
 import {SCORES, TIME, YOUR_RESULTS} from "../translation";
 import "../../css/PresenceScores.css"
 import "../../css/Scores.css"
-
+import withScoreHOC from './ScoreHoc';
 
 class Scores extends Component {
 
@@ -15,10 +15,14 @@ class Scores extends Component {
 
         let {intl} = this.props;
         this.intl = intl;
+        this.modes = {
+            SCORE: 'score',
+            TIME: 'time',
+            DETAILS: 'details'
+        };
 
         this.state = {
-            score: true,
-            time: false,
+            mode: this.modes.SCORE,
             chartScores: {
                 chartData: {},
                 options: {
@@ -92,11 +96,11 @@ class Scores extends Component {
 
     componentDidMount() {
         if (this.props.location) {
-            const {userScore, userTime, noOfQuestions, exercise} = this.props.location.state;
+            const {userScore, userTime, noOfQuestions, exercise, userAnswers} = this.props.location.state;
             let score = Math.ceil(userScore / noOfQuestions * 100);
             let time = Math.ceil(userTime / 60);
             if (this.props.isShared) {
-                this.props.onSharedResult(exercise.id, score, time);
+                this.props.onSharedResult(exercise.id, score, time, userAnswers);
             }
             this.setChart();
         }
@@ -163,8 +167,7 @@ class Scores extends Component {
 
     score = () => {
         this.setState({
-            score: true,
-            time: false
+            mode: this.modes.SCORE
         }, () => {
             this.setChart();
         })
@@ -172,31 +175,84 @@ class Scores extends Component {
 
     time = () => {
         this.setState({
-            score: false,
-            time: true
+            mode: this.modes.TIME
         }, () => {
             this.setChart();
         })
     };
 
+    details = (event) => {
+        if(event.length!==0) {
+            if(this.props.location.state.type === 'MCQ'){
+                this.setState({
+                    mode: this.modes.DETAILS
+                })
+            }
+        }
+    };
+
     render() {
+        const {getResultsTableElement, getWrongRightMarker} = this.props;
         let score_active = "";
         let time_active = "";
+        let chart = "";
 
-        if (this.state.score)
+        if (this.state.mode === this.modes.SCORE){
             score_active = "active";
-        else
+            chart = (<Bar data={this.state.chartScores.chartData} getElementAtEvent={this.details} options={this.state.chartScores.options}/>);
+        }
+        else if (this.state.mode === this.modes.TIME) {
             time_active = "active";
+            chart = (<Bar data={this.state.chartTimes.chartData} options={this.state.chartTimes.options}/>);
+        }
+        else if (this.state.mode === this.modes.DETAILS) {
+            
+            const {userAnswers} = this.props.location.state;
+
+            let resultDetails = userAnswers.map((answer, index) => {
+                return (
+                    <tr key={index}>
+                        <td>
+                            {getResultsTableElement(answer.question)}
+                        </td>
+                        <td>
+                            {getResultsTableElement(answer.correctAns)}
+                        </td> 
+                        <td>
+                            <div className="td-userans">
+                                {getResultsTableElement(answer.userAns)}
+                                <div style={{marginLeft: '5px'}}>
+                                    {getWrongRightMarker(answer)}
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                );
+            });
+
+            chart = (
+                <div>
+                    <br></br>
+                    <br></br>
+                    <table className="w-100">
+                        <thead>
+                            <tr>
+                                <th>Question</th>
+                                <th>Correct Answer</th> 
+                                <th>Your Answer</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {resultDetails}
+                        </tbody> 
+                    </table>
+                </div>
+            );
+
+        }
 
         let score = (<button type="button" className={"score-button " + score_active} onClick={this.score}/>);
         let time = (<button type="button" className={"time-button " + time_active} onClick={this.time}/>);
-
-        let chart = "";
-
-        if (this.state.score)
-            chart = (<Bar data={this.state.chartScores.chartData} options={this.state.chartScores.options}/>);
-        else
-            chart = (<Bar data={this.state.chartTimes.chartData} options={this.state.chartTimes.options}/>);
 
         return (
             <div className="container">
@@ -223,5 +279,5 @@ function MapStateToProps(state) {
     }
 }
 
-export default injectIntl(withRouter(
-    connect(MapStateToProps, {})(Scores)));
+export default withScoreHOC()(injectIntl(withRouter(
+    connect(MapStateToProps, {})(Scores))));
